532f1d5cd292a940b75c5f877e8a35cdd826de02
Marc MERLIN <marc_soft@merlins.org>
2017-04-06 12:20:12 -0700

    New demo.

---
+++ examples/MatrixGFXDemo/MatrixGFXDemo.ino
@@ -0,0 +1,462 @@
+// Adafruit_NeoMatrix example for single NeoPixel Shield.
+// By Marc MERLIN <marc_soft@merlins.org>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef PSTR
+ #define PSTR // Make Arduino Due happy
+#endif
+
+#define PIN 6
+
+// MATRIX DECLARATION:
+// Parameter 1 = width of EACH NEOPIXEL MATRIX (not total display)
+// Parameter 2 = height of each matrix
+// Parameter 3 = number of matrices arranged horizontally
+// Parameter 4 = number of matrices arranged vertically
+// Parameter 5 = pin number (most are valid)
+// Parameter 6 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the FIRST MATRIX; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs WITHIN EACH MATRIX are
+//     arranged in horizontal rows or in vertical columns, respectively;
+//     pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns WITHIN
+//     EACH MATRIX proceed in the same order, or alternate lines reverse
+//     direction; pick one.
+//   NEO_TILE_TOP, NEO_TILE_BOTTOM, NEO_TILE_LEFT, NEO_TILE_RIGHT:
+//     Position of the FIRST MATRIX (tile) in the OVERALL DISPLAY; pick
+//     two, e.g. NEO_TILE_TOP + NEO_TILE_LEFT for the top-left corner.
+//   NEO_TILE_ROWS, NEO_TILE_COLUMNS: the matrices in the OVERALL DISPLAY
+//     are arranged in horizontal rows or in vertical columns, respectively;
+//     pick one or the other.
+//   NEO_TILE_PROGRESSIVE, NEO_TILE_ZIGZAG: the ROWS/COLUMS OF MATRICES
+//     (tiles) in the OVERALL DISPLAY proceed in the same order for every
+//     line, or alternate lines reverse direction; pick one.  When using
+//     zig-zag order, the orientation of the matrices in alternate rows
+//     will be rotated 180 degrees (this is normal -- simplifies wiring).
+//   See example below for these values in action.
+// Parameter 7 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 pixels)
+//   NEO_GRB     Pixels are wired for GRB bitstream (v2 pixels)
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA v1 pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
+
+Adafruit_NeoMatrix *matrix = new Adafruit_NeoMatrix(16, 8, PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+  NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix->Color(255, 0, 0), matrix->Color(0, 255, 0), matrix->Color(0, 0, 255) };
+
+// This could also be defined as matrix->color(255,0,0) but those defines
+// are meant to work for adafruit_gfx backends that are lacking color()
+#define LED_RED_VERYLOW 	(3 <<  11)
+#define LED_RED_LOW 		(7 <<  11)
+#define LED_RED_MEDIUM 		(15 << 11)
+#define LED_RED_HIGH 		(31 << 11)
+
+#define LED_GREEN_VERYLOW	(1 <<  5)   
+#define LED_GREEN_LOW 		(15 << 5)  
+#define LED_GREEN_MEDIUM 	(31 << 5)  
+#define LED_GREEN_HIGH 		(63 << 5)  
+
+#define LED_BLUE_VERYLOW	3
+#define LED_BLUE_LOW 		7
+#define LED_BLUE_MEDIUM 	15
+#define LED_BLUE_HIGH 		31
+
+#define LED_ORANGE_VERYLOW	(LED_RED_VERYLOW + LED_GREEN_VERYLOW)
+#define LED_ORANGE_LOW		(LED_RED_LOW     + LED_GREEN_LOW)
+#define LED_ORANGE_MEDIUM	(LED_RED_MEDIUM  + LED_GREEN_MEDIUM)
+#define LED_ORANGE_HIGH		(LED_RED_HIGH    + LED_GREEN_HIGH)
+
+#define LED_PURPLE_VERYLOW	(LED_RED_VERYLOW + LED_BLUE_VERYLOW)
+#define LED_PURPLE_LOW		(LED_RED_LOW     + LED_BLUE_LOW)
+#define LED_PURPLE_MEDIUM	(LED_RED_MEDIUM  + LED_BLUE_MEDIUM)
+#define LED_PURPLE_HIGH		(LED_RED_HIGH    + LED_BLUE_HIGH)
+
+#define LED_CYAN_VERYLOW	(LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
+#define LED_CYAN_LOW		(LED_GREEN_LOW     + LED_BLUE_LOW)
+#define LED_CYAN_MEDIUM		(LED_GREEN_MEDIUM  + LED_BLUE_MEDIUM)
+#define LED_CYAN_HIGH		(LED_GREEN_HIGH    + LED_BLUE_HIGH)
+
+#define LED_WHITE_VERYLOW	(LED_RED_VERYLOW + LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
+#define LED_WHITE_LOW		(LED_RED_LOW     + LED_GREEN_LOW     + LED_BLUE_LOW)
+#define LED_WHITE_MEDIUM	(LED_RED_MEDIUM  + LED_GREEN_MEDIUM  + LED_BLUE_MEDIUM)
+#define LED_WHITE_HIGH		(LED_RED_HIGH    + LED_GREEN_HIGH    + LED_BLUE_HIGH)
+
+typedef enum {
+    FOUR_WHITE = 0,
+    BITMAP = 1,
+    RGB_BITMAP = 2,
+    LINES = 3,
+    BOXES = 4,
+    CIRCLES = 5,
+    GREENSCROLLTEXT = 6,
+    ORANGESCROLLTEXT = 7,
+} MatrixDisplay;
+
+static const uint8_t PROGMEM
+    mono_bmp[][8] =
+    {
+
+	{   B00111100,
+	    B01000010,
+	    B10100101,
+	    B10000001,
+	    B10100101,
+	    B10011001,
+	    B01000010,
+	    B00111100 },
+
+	{   B00111100,
+	    B01000010,
+	    B10100101,
+	    B10000001,
+	    B10111101,
+	    B10000001,
+	    B01000010,
+	    B00111100 },
+
+	{   B00111100,
+	    B01000010,
+	    B10100101,
+	    B10000001,
+	    B10011001,
+	    B10100101,
+	    B01000010,
+	    B00111100 },
+
+	{   
+	    B10101010,
+	    B01010101,
+	    B10101010,
+	    B01010101,
+	    B10101010,
+	    B01010101,
+	    B10101010,
+	    B01010101,
+			},
+
+	{   
+	    B01010101,
+	    B10101010,
+	    B01010101,
+	    B10101010,
+	    B01010101,
+	    B10101010,
+	    B01010101,
+	    B10101010,
+			},
+    };
+
+static const uint16_t PROGMEM
+    // These bitmaps were written for a backend that only supported
+    // 4 bits per color with Blue/Green/Red ordering while neomatrix
+    // uses native 565 color mapping as RGB.  
+    // I'm leaving the arrays as is because it's easier to read
+    // which color is what when separated on a 4bit boundary
+    // The demo code will modify the arrays at runtime to be compatible
+    // with the neomatrix color ordering and bit depth.
+    RGB_bmp[][64] = {
+      // 00: blue, blue/red, red, red/green, green, green/blue, blue, white
+      {	0x100, 0x200, 0x300, 0x400, 0x600, 0x800, 0xA00, 0xF00, 
+	0x101, 0x202, 0x303, 0x404, 0x606, 0x808, 0xA0A, 0xF0F, 
+      	0x001, 0x002, 0x003, 0x004, 0x006, 0x008, 0x00A, 0x00F, 
+	0x011, 0x022, 0x033, 0x044, 0x066, 0x088, 0x0AA, 0x0FF, 
+	0x010, 0x020, 0x030, 0x040, 0x060, 0x080, 0x0A0, 0x0F0, 
+	0x110, 0x220, 0x330, 0x440, 0x660, 0x880, 0xAA0, 0xFF0, 
+	0x100, 0x200, 0x300, 0x400, 0x600, 0x800, 0xA00, 0xF00, 
+	0x111, 0x222, 0x333, 0x444, 0x666, 0x888, 0xAAA, 0xFFF, },
+
+      // 01: grey to white
+      {	0x111, 0x222, 0x333, 0x555, 0x777, 0x999, 0xAAA, 0xFFF, 
+	0x222, 0x222, 0x333, 0x555, 0x777, 0x999, 0xAAA, 0xFFF, 
+	0x333, 0x333, 0x333, 0x555, 0x777, 0x999, 0xAAA, 0xFFF, 
+	0x555, 0x555, 0x555, 0x555, 0x777, 0x999, 0xAAA, 0xFFF, 
+	0x777, 0x777, 0x777, 0x777, 0x777, 0x999, 0xAAA, 0xFFF, 
+	0x999, 0x999, 0x999, 0x999, 0x999, 0x999, 0xAAA, 0xFFF, 
+	0xAAA, 0xAAA, 0xAAA, 0xAAA, 0xAAA, 0xAAA, 0xAAA, 0xFFF, 
+	0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF, 0xFFF, },
+
+      // 02: low red to high red
+      {	0x001, 0x002, 0x003, 0x005, 0x007, 0x009, 0x00A, 0x00F, 
+	0x002, 0x002, 0x003, 0x005, 0x007, 0x009, 0x00A, 0x00F, 
+	0x003, 0x003, 0x003, 0x005, 0x007, 0x009, 0x00A, 0x00F, 
+	0x005, 0x005, 0x005, 0x005, 0x007, 0x009, 0x00A, 0x00F, 
+	0x007, 0x007, 0x007, 0x007, 0x007, 0x009, 0x00A, 0x00F, 
+	0x009, 0x009, 0x009, 0x009, 0x009, 0x009, 0x00A, 0x00F, 
+	0x00A, 0x00A, 0x00A, 0x00A, 0x00A, 0x00A, 0x00A, 0x00F, 
+	0x00F, 0x00F, 0x00F, 0x00F, 0x00F, 0x00F, 0x00F, 0x00F, },
+
+      // 03: low green to high green
+      {	0x010, 0x020, 0x030, 0x050, 0x070, 0x090, 0x0A0, 0x0F0, 
+	0x020, 0x020, 0x030, 0x050, 0x070, 0x090, 0x0A0, 0x0F0, 
+	0x030, 0x030, 0x030, 0x050, 0x070, 0x090, 0x0A0, 0x0F0, 
+	0x050, 0x050, 0x050, 0x050, 0x070, 0x090, 0x0A0, 0x0F0, 
+	0x070, 0x070, 0x070, 0x070, 0x070, 0x090, 0x0A0, 0x0F0, 
+	0x090, 0x090, 0x090, 0x090, 0x090, 0x090, 0x0A0, 0x0F0, 
+	0x0A0, 0x0A0, 0x0A0, 0x0A0, 0x0A0, 0x0A0, 0x0A0, 0x0F0, 
+	0x0F0, 0x0F0, 0x0F0, 0x0F0, 0x0F0, 0x0F0, 0x0F0, 0x0F0, },
+
+      // 04: low blue to high blue
+      {	0x100, 0x200, 0x300, 0x500, 0x700, 0x900, 0xA00, 0xF00, 
+	0x200, 0x200, 0x300, 0x500, 0x700, 0x900, 0xA00, 0xF00, 
+	0x300, 0x300, 0x300, 0x500, 0x700, 0x900, 0xA00, 0xF00, 
+	0x500, 0x500, 0x500, 0x500, 0x700, 0x900, 0xA00, 0xF00, 
+	0x700, 0x700, 0x700, 0x700, 0x700, 0x900, 0xA00, 0xF00, 
+	0x900, 0x900, 0x900, 0x900, 0x900, 0x900, 0xA00, 0xF00, 
+	0xA00, 0xA00, 0xA00, 0xA00, 0xA00, 0xA00, 0xA00, 0xF00, 
+	0xF00, 0xF00, 0xF00, 0xF00, 0xF00, 0xF00, 0xF00, 0xF00, },
+
+      // 05: 1 black, 2R, 2O, 2G, 1B with 4 blue lines rising right
+      {	0x000, 0x200, 0x000, 0x400, 0x000, 0x800, 0x000, 0xF00, 
+      	0x000, 0x201, 0x002, 0x403, 0x004, 0x805, 0x006, 0xF07, 
+	0x008, 0x209, 0x00A, 0x40B, 0x00C, 0x80D, 0x00E, 0xF0F, 
+	0x000, 0x211, 0x022, 0x433, 0x044, 0x855, 0x066, 0xF77, 
+	0x088, 0x299, 0x0AA, 0x4BB, 0x0CC, 0x8DD, 0x0EE, 0xFFF, 
+	0x000, 0x210, 0x020, 0x430, 0x040, 0x850, 0x060, 0xF70, 
+	0x080, 0x290, 0x0A0, 0x4B0, 0x0C0, 0x8D0, 0x0E0, 0xFF0,
+	0x000, 0x200, 0x000, 0x500, 0x000, 0x800, 0x000, 0xF00, },
+
+      // 06: 4 lines of increasing red and then green
+      { 0x000, 0x000, 0x001, 0x001, 0x002, 0x002, 0x003, 0x003, 
+	0x004, 0x004, 0x005, 0x005, 0x006, 0x006, 0x007, 0x007, 
+	0x008, 0x008, 0x009, 0x009, 0x00A, 0x00A, 0x00B, 0x00B, 
+	0x00C, 0x00C, 0x00D, 0x00D, 0x00E, 0x00E, 0x00F, 0x00F, 
+	0x000, 0x000, 0x010, 0x010, 0x020, 0x020, 0x030, 0x030, 
+	0x040, 0x040, 0x050, 0x050, 0x060, 0x060, 0x070, 0x070, 
+	0x080, 0x080, 0x090, 0x090, 0x0A0, 0x0A0, 0x0B0, 0x0B0, 
+	0x0C0, 0x0C0, 0x0D0, 0x0D0, 0x0E0, 0x0E0, 0x0F0, 0x0F0, },
+
+      // 07: 4 lines of increasing red and then blue
+      { 0x000, 0x000, 0x001, 0x001, 0x002, 0x002, 0x003, 0x003, 
+	0x004, 0x004, 0x005, 0x005, 0x006, 0x006, 0x007, 0x007, 
+	0x008, 0x008, 0x009, 0x009, 0x00A, 0x00A, 0x00B, 0x00B, 
+	0x00C, 0x00C, 0x00D, 0x00D, 0x00E, 0x00E, 0x00F, 0x00F, 
+	0x000, 0x000, 0x100, 0x100, 0x200, 0x200, 0x300, 0x300, 
+	0x400, 0x400, 0x500, 0x500, 0x600, 0x600, 0x700, 0x700, 
+	0x800, 0x800, 0x900, 0x900, 0xA00, 0xA00, 0xB00, 0xB00, 
+	0xC00, 0xC00, 0xD00, 0xD00, 0xE00, 0xE00, 0xF00, 0xF00, },
+
+      // 08: criss cross of green and red with diagonal blue.
+      {	0xF00, 0x001, 0x003, 0x005, 0x007, 0x00A, 0x00F, 0x000, 
+	0x020, 0xF21, 0x023, 0x025, 0x027, 0x02A, 0x02F, 0x020, 
+	0x040, 0x041, 0xF43, 0x045, 0x047, 0x04A, 0x04F, 0x040, 
+	0x060, 0x061, 0x063, 0xF65, 0x067, 0x06A, 0x06F, 0x060, 
+	0x080, 0x081, 0x083, 0x085, 0xF87, 0x08A, 0x08F, 0x080, 
+	0x0A0, 0x0A1, 0x0A3, 0x0A5, 0x0A7, 0xFAA, 0x0AF, 0x0A0, 
+	0x0F0, 0x0F1, 0x0F3, 0x0F5, 0x0F7, 0x0FA, 0xFFF, 0x0F0, 
+	0x000, 0x001, 0x003, 0x005, 0x007, 0x00A, 0x00F, 0xF00, },
+
+      // 09: 2 lines of green, 2 red, 2 orange, 2 green
+      { 0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
+	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
+	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
+	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
+	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
+	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
+	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, 
+	0x0F0, 0x0F0, 0x0FF, 0x0FF, 0x00F, 0x00F, 0x0F0, 0x0F0, },
+
+      // 10: multicolor smiley face
+      { 0x000, 0x000, 0x00F, 0x00F, 0x00F, 0x00F, 0x000, 0x000, 
+	0x000, 0x00F, 0x000, 0x000, 0x000, 0x000, 0x00F, 0x000, 
+	0x00F, 0x000, 0xF00, 0x000, 0x000, 0xF00, 0x000, 0x00F, 
+	0x00F, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000, 0x00F, 
+	0x00F, 0x000, 0x0F0, 0x000, 0x000, 0x0F0, 0x000, 0x00F, 
+	0x00F, 0x000, 0x000, 0x0F4, 0x0F3, 0x000, 0x000, 0x00F, 
+	0x000, 0x00F, 0x000, 0x000, 0x000, 0x000, 0x00F, 0x000, 
+	0x000, 0x000, 0x00F, 0x00F, 0x00F, 0x00F, 0x000, 0x000, },
+};
+
+
+void setup() {
+    Serial.begin(115200);
+    matrix->begin();
+    matrix->setTextWrap(false);
+    matrix->setBrightness(255);
+    matrix->setTextColor(colors[0]);
+    matrix->fillScreen(LED_ORANGE_HIGH);
+    matrix->show();
+    delay(1000);
+}
+
+int x    = matrix->width();
+int pass = 0;
+
+#if 0
+void loop() {
+  matrix->fillScreen(0);
+  matrix->setCursor(x, 0);
+  matrix->print(F("Howdy"));
+  if(--x < -36) {
+    x = matrix->width();
+    if(++pass >= 3) pass = 0;
+    matrix->setTextColor(colors[pass]);
+  }
+  matrix->show();
+  delay(50);
+}
+#endif
+
+
+
+void fixdrawRGBBitmap(int16_t x, int16_t y, const uint16_t *bitmap, int16_t w, int16_t h) {
+    uint16_t RGB_bmp_fixed[w * h];
+    for (uint16_t pixel=0; pixel<w*h; pixel++) {
+	uint8_t r,g,b;
+	uint16_t color = pgm_read_word(bitmap + pixel);
+
+	//Serial.print(color, HEX);
+	b = (color & 0xF00) >> 8;
+	g = (color & 0x0F0) >> 4;
+	r = color & 0x00F;
+	//Serial.print(" ");
+	//Serial.print(b);
+	//Serial.print("/");
+	//Serial.print(g);
+	//Serial.print("/");
+	//Serial.print(r);
+	//Serial.print(" -> ");
+	// expand from 4/4/4 bits per color to 5/6/5
+	b = map(b, 0, 15, 0, 31);
+	g = map(g, 0, 15, 0, 63);
+	r = map(r, 0, 15, 0, 31);
+	//Serial.print(r);
+	//Serial.print("/");
+	//Serial.print(g);
+	//Serial.print("/");
+	//Serial.print(b);
+	RGB_bmp_fixed[pixel] = (r << 11) + (g << 5) + b;
+	//Serial.print(" -> ");
+	//Serial.println(RGB_bmp_fixed[pixel], HEX);
+    }
+    // Non color fixed, progmem read.
+    //matrix->drawRGBBitmap(x, y, bitmap, w, h, true);
+    // Color fixed, direct read from RAM.
+    matrix->drawRGBBitmap(x, y, RGB_bmp_fixed, w, h, false);
+}
+
+void display_matrix(MatrixDisplay choice, uint8_t bmp_num=0, uint16_t color=0) {
+    matrix->clear();
+
+    switch (choice) {
+ 
+    case FOUR_WHITE:
+        matrix->fillRect(0,0, 8,8, LED_WHITE_HIGH);
+        matrix->drawRect(1,1, 6,6, LED_WHITE_MEDIUM);
+        matrix->drawRect(2,2, 4,4, LED_WHITE_LOW);
+        matrix->drawRect(3,3, 2,2, LED_WHITE_VERYLOW);
+        break;
+
+    case BITMAP:
+	matrix->drawBitmap(0, 0, mono_bmp[bmp_num], 8, 8, color);
+        break;
+ 
+    case RGB_BITMAP:
+	fixdrawRGBBitmap(0, 0, RGB_bmp[bmp_num], 8, 8);
+        break;
+
+    case LINES:
+	// Without a matrix like above, a red/green crossing isn't as good.
+	matrix->drawLine(0,2, 7,2, LED_RED_VERYLOW);
+	matrix->drawLine(0,3, 7,3, LED_RED_LOW);
+	matrix->drawLine(0,4, 7,4, LED_RED_MEDIUM);
+	matrix->drawLine(0,5, 7,5, LED_RED_HIGH);
+	matrix->drawLine(2,0, 2,7, LED_GREEN_VERYLOW);
+	matrix->drawLine(3,0, 3,7, LED_GREEN_LOW);
+	matrix->drawLine(4,0, 4,7, LED_GREEN_MEDIUM);
+	matrix->drawLine(5,0, 5,7, LED_GREEN_HIGH);
+	matrix->drawLine(0,0, 7,7, LED_BLUE_HIGH);
+        break;
+
+    case BOXES:
+	matrix->drawRect(0,0, 8,8, LED_BLUE_HIGH);
+	matrix->drawRect(1,1, 6,6, LED_GREEN_MEDIUM);
+	matrix->fillRect(2,2, 4,4, LED_RED_HIGH);
+        break;
+ 
+    case CIRCLES:
+	matrix->drawCircle(2,2, 2, LED_RED_MEDIUM);
+	matrix->drawCircle(5,5, 2, LED_BLUE_HIGH);
+	matrix->drawCircle(1,6, 1, LED_GREEN_LOW);
+	matrix->drawCircle(6,1, 1, LED_GREEN_HIGH);
+        break;
+
+    case GREENSCROLLTEXT:
+	matrix->setTextWrap(false);  // we don't wrap text so it scrolls nicely
+	matrix->setTextSize(1);
+	matrix->setTextColor(LED_GREEN_HIGH);
+	matrix->setRotation(3);
+	for (int8_t x=7; x>=-36; x--) {
+	    matrix->clear();
+	    matrix->setCursor(x,0);
+	    matrix->print("Hello");
+	    matrix->show();
+	   delay(50);
+	}
+        break;
+    
+    case ORANGESCROLLTEXT:
+	matrix->setRotation(0);
+	matrix->setTextColor(LED_ORANGE_HIGH);
+	for (int8_t x=7; x>=-36; x--) {
+	    matrix->clear();
+	    matrix->setCursor(x,0);
+	    matrix->print("World");
+	    matrix->show();
+	   delay(50);
+	}
+        break;
+
+    }
+    matrix->show();
+}
+
+void loop() {
+    uint16_t bmpcolor[] = { LED_GREEN_HIGH, LED_BLUE_HIGH, LED_RED_HIGH };
+    uint16_t scandelay[] = { 5, 50, 200, 1000 };
+
+    fixdrawRGBBitmap(0, 0, RGB_bmp[0], 8, 8);
+    delay(1000);
+
+    // Cycle through red, green, blue, display 2 checkered patterns
+    for (uint8_t i=0; i<3; i++)
+    {
+	display_matrix(BITMAP, 3, bmpcolor[i]);
+ 	delay(500);
+	display_matrix(BITMAP, 4, bmpcolor[i]);
+ 	delay(500);
+    }
+
+    for (uint8_t i=0; i<=(sizeof(RGB_bmp)/sizeof(RGB_bmp[0])-1); i++)
+    {
+	display_matrix(RGB_BITMAP, i);
+ 	delay(3000);
+    }
+
+    display_matrix(FOUR_WHITE, 0);
+    delay(3000);
+
+    for (uint8_t i=0; i<=2; i++)
+    {
+	display_matrix(BITMAP, i, bmpcolor[i]);
+ 	delay(3000);
+    }
+
+    display_matrix(LINES);
+    delay(3000);
+
+    display_matrix(BOXES);
+    delay(3000);
+
+
+    display_matrix(GREENSCROLLTEXT);
+
+    display_matrix(ORANGESCROLLTEXT);
+}
