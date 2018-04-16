27bafc0dd5b90f8b0eb81ae8d5c9256e6b06fe66
Marc MERLIN <marc_soft@merlins.org>
2018-04-15 17:43:03 -0700

    Added clockless_block_esp8266.h to WAIT_TIME.

--- FastLED_NeoMatrix.h
+++ FastLED_NeoMatrix.h
@@ -29,7 +29,8 @@
 #endif
 #include <Adafruit_GFX.h>
 #if defined(ESP8266)
-// If you get matrix flickering, modify platforms/esp/8266/clockless_esp8266.h to change WAIT_TIME
+// If you get matrix flickering, modify platforms/esp/8266/clockless_esp8266.h 
+// and platforms/esp/8266/clockless_block_esp8266.h to change WAIT_TIME to 20
 #pragma message "If you get matrix corruption, turn off FASTLED_ALLOW_INTERRUPTS"
 #pragma message "in this library, or modify WAIT_TIME in platforms/esp/8266/clockless_esp8266.h"
 #pragma message "(raise it from 5 to 20 or up to 50 if needed)"
