5546eb63232fbc5e97dc64fd6b8ea334c17c33c1
Marc MERLIN <marc_soft@merlins.org>
2019-05-26 01:43:56 -0700

    Moved all common backend code to Framebuffer_GFX.

--- FastLED_NeoMatrix.cpp
+++ FastLED_NeoMatrix.cpp
@@ -36,236 +36,39 @@
 
 #include <Adafruit_GFX.h>
 #include <FastLED_NeoMatrix.h>
-#include "gamma.h"
-#ifdef __AVR__
- #include <avr/pgmspace.h>
-#elif defined(ESP8266)
- #include <pgmspace.h>
-#else
- #ifndef pgm_read_byte
-  #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
- #endif
-#endif
-
-#ifndef _swap_uint16_t
-#define _swap_uint16_t(a, b) { uint16_t t = a; a = b; b = t; }
-#endif
-
-
 
 // Constructor for single matrix:
-FastLED_NeoMatrix::FastLED_NeoMatrix(CRGB *leds, uint8_t w, uint8_t h, 
-    uint8_t matrixType): 
-  Adafruit_GFX(w, h),
-  type(matrixType), matrixWidth(w), matrixHeight(h), tilesX(0), tilesY(0), 
-  remapFn(NULL) { 
-    _leds = leds;
-    // WARNING: Serial.print seems to crash in the constructor, 
-    // but works in begin()
-    numpix = matrixWidth * matrixHeight;
+FastLED_NeoMatrix::FastLED_NeoMatrix(CRGB *leds, uint8_t w, uint8_t h, uint8_t matrixType): 
+  Framebuffer_GFX(leds, w, h, NULL) {
+    type = matrixType;
+    tilesX = 0;
+    tilesY = 0;
   }
 
 // Constructor for tiled matrices:
 FastLED_NeoMatrix::FastLED_NeoMatrix(CRGB *leds, uint8_t mW, uint8_t mH, 
     uint8_t tX, uint8_t tY, uint8_t matrixType) :
-  Adafruit_GFX(mW * tX, mH * tY),
-  type(matrixType), matrixWidth(mW), matrixHeight(mH), tilesX(tX), tilesY(tY), 
-  remapFn(NULL) { 
-    _leds = leds;
-    numpix = matrixWidth * matrixHeight * tilesX * tilesY;
- }
-
-void FastLED_NeoMatrix::begin() {
-  Serial.print("Num Pixels: ");
-  Serial.println(numpix);
+  Framebuffer_GFX(leds, mW * tX, mH * tY, NULL) {
+    matrixWidth = mW;
+    matrixHeight = mH;
+    type = matrixType;
+    tilesX = tX;
+    tilesY = tY;
 }
 
-// Expand 16-bit input color (Adafruit_GFX colorspace) to 24-bit (NeoPixel)
-// (w/gamma adjustment)
-static uint32_t expandColor(uint16_t color) {
-  return ((uint32_t)pgm_read_byte(&gamma5[ color >> 11       ]) << 16) |
-         ((uint32_t)pgm_read_byte(&gamma6[(color >> 5) & 0x3F]) <<  8) |
-                    pgm_read_byte(&gamma5[ color       & 0x1F]);
+void FastLED_NeoMatrix::show() {
+  if (_show) { _show(); return; };
+  #ifdef ESP8266
+// Disable watchdog interrupt so that it does not trigger in the middle of
+// updates. and break timing of pixels, causing random corruption on interval
+// https://github.com/esp8266/Arduino/issues/34
+    ESP.wdtDisable();
+#endif
+    FastLED.show();
+#ifdef ESP8266
+    ESP.wdtEnable(1000);
+#endif
 }
 
-// Downgrade 24-bit color to 16-bit (add reverse gamma lookup here?)
-uint16_t FastLED_NeoMatrix::Color(uint8_t r, uint8_t g, uint8_t b) {
-  return ((uint16_t)(r & 0xF8) << 8) |
-         ((uint16_t)(g & 0xFC) << 3) |
-                    (b         >> 3);
-}
-
-// Pass-through is a kludge that lets you override the current drawing
-// color with a 'raw' RGB (or RGBW) value that's issued directly to
-// pixel(s), side-stepping the 16-bit color limitation of Adafruit_GFX.
-// This is not without some limitations of its own -- for example, it
-// won't work in conjunction with the background color feature when
-// drawing text or bitmaps (you'll just get a solid rect of color),
-// only 'transparent' text/bitmaps.  Also, no gamma correction.
-// Remember to UNSET the passthrough color immediately when done with
-// it (call with no value)!
-
-// Pass raw color value to set/enable passthrough
-void FastLED_NeoMatrix::setPassThruColor(CRGB c) {
-  passThruColor = c.r*65536+c.g*256+c.b;
-  passThruFlag  = true;
-}
-
-void FastLED_NeoMatrix::setPassThruColor(uint32_t c) {
-  passThruColor = c;
-  passThruFlag  = true;
-}
-
-// Call without a value to reset (disable passthrough)
-void FastLED_NeoMatrix::setPassThruColor(void) {
-  passThruFlag = false;
-}
-
-int FastLED_NeoMatrix::XY(int16_t x, int16_t y) {
-
-  // If you send an out of bounds value, you get an special result 
-  // pointing to the last pixel. It doesn't look great, but better
-  // than crashing. Still, fix the upstream code.
-  // DrawPixel is able to reject the write, but here we have to return an index
-  // which has to be inbounds.
-  if((x < 0) || (y < 0) || (x >= _width) || (y >= _height)) return numpix-1;
-
-  int16_t t;
-  switch(rotation) {
-   case 1:
-    t = x;
-    x = WIDTH  - 1 - y;
-    y = t;
-    break;
-   case 2:
-    x = WIDTH  - 1 - x;
-    y = HEIGHT - 1 - y;
-    break;
-   case 3:
-    t = x;
-    x = y;
-    y = HEIGHT - 1 - t;
-    break;
-  }
-
-  int tileOffset = 0, pixelOffset;
-
-  if(remapFn) { // Custom X/Y remapping function
-    pixelOffset = (*remapFn)(x, y);
-  } else {      // Standard single matrix or tiled matrices
-
-    uint8_t  corner = type & NEO_MATRIX_CORNER;
-    uint16_t minor, major, majorScale;
-
-    if(tilesX) { // Tiled display, multiple matrices
-      uint16_t tile;
-
-      minor = x / matrixWidth;            // Tile # X/Y; presume row major to
-      major = y / matrixHeight,           // start (will swap later if needed)
-      x     = x - (minor * matrixWidth);  // Pixel X/Y within tile
-      y     = y - (major * matrixHeight); // (-* is less math than modulo)
-
-      // Determine corner of entry, flip axes if needed
-      if(type & NEO_TILE_RIGHT)  minor = tilesX - 1 - minor;
-      if(type & NEO_TILE_BOTTOM) major = tilesY - 1 - major;
-
-      // Determine actual major axis of tiling
-      if((type & NEO_TILE_AXIS) == NEO_TILE_ROWS) {
-        majorScale = tilesX;
-      } else {
-        _swap_uint16_t(major, minor);
-        majorScale = tilesY;
-      }
-
-      // Determine tile number
-      if((type & NEO_TILE_SEQUENCE) == NEO_TILE_PROGRESSIVE) {
-        // All tiles in same order
-        tile = major * majorScale + minor;
-      } else {
-        // Zigzag; alternate rows change direction.  On these rows,
-        // this also flips the starting corner of the matrix for the
-        // pixel math later.
-        if(major & 1) {
-          corner ^= NEO_MATRIX_CORNER;
-          tile = (major + 1) * majorScale - 1 - minor;
-        } else {
-          tile =  major      * majorScale     + minor;
-        }
-      }
-
-      // Index of first pixel in tile
-      tileOffset = tile * matrixWidth * matrixHeight;
-
-    } // else no tiling (handle as single tile)
-
-    // Find pixel number within tile
-    minor = x; // Presume row major to start (will swap later if needed)
-    major = y;
-
-    // Determine corner of entry, flip axes if needed
-    if(corner & NEO_MATRIX_RIGHT)  minor = matrixWidth  - 1 - minor;
-    if(corner & NEO_MATRIX_BOTTOM) major = matrixHeight - 1 - major;
-
-    // Determine actual major axis of matrix
-    if((type & NEO_MATRIX_AXIS) == NEO_MATRIX_ROWS) {
-      majorScale = matrixWidth;
-    } else {
-      _swap_uint16_t(major, minor);
-      majorScale = matrixHeight;
-    }
-
-    // Determine pixel number within tile/matrix
-    if((type & NEO_MATRIX_SEQUENCE) == NEO_MATRIX_PROGRESSIVE) {
-      // All lines in same order
-      pixelOffset = major * majorScale + minor;
-    } else {
-      // Zigzag; alternate rows change direction.
-      if(major & 1) pixelOffset = (major + 1) * majorScale - 1 - minor;
-      else          pixelOffset =  major      * majorScale     + minor;
-    }
-  }
-
-  return(tileOffset + pixelOffset);
-}
-
-void FastLED_NeoMatrix::drawPixel(int16_t x, int16_t y, uint16_t color) {
-
-  if((x < 0) || (y < 0) || (x >= _width) || (y >= _height)) return;
-
-  _leds[XY(x,y)] = passThruFlag ? passThruColor : expandColor(color);
-}
-
-void FastLED_NeoMatrix::drawPixel(int16_t x, int16_t y, uint32_t color) {
-
-  if((x < 0) || (y < 0) || (x >= _width) || (y >= _height)) return;
-
-  _leds[XY(x,y)] = color;
-}
-
-void FastLED_NeoMatrix::drawPixel(int16_t x, int16_t y, CRGB c) {
-
-  if((x < 0) || (y < 0) || (x >= _width) || (y >= _height)) return;
-
-  _leds[XY(x,y)] = c.r*65536+c.g*256+c.b;
-}
-
-void FastLED_NeoMatrix::fillScreen(uint16_t color) {
-  uint32_t c;
-
-  c = passThruFlag ? passThruColor : expandColor(color);
-  for (uint16_t i=0; i<numpix; i++) { _leds[i]=c; }
-}
-
-void FastLED_NeoMatrix::setRemapFunction(uint16_t (*fn)(uint16_t, uint16_t)) {
-  remapFn = fn;
-}
-
-// After setting gamma, this is used with
-//  CRGB color = CRGB(matrix->gamma[red], matrix->gamma[green], matrix->gamma[blue]);
-void FastLED_NeoMatrix::precal_gamma(float gam) {
-  for (uint8_t i =0; i<255; i++) {
-    gamma[i] = applyGamma_video(i, gam);
-  }
-}
 
 // vim:sts=2:sw=2
--- FastLED_NeoMatrix.h
+++ FastLED_NeoMatrix.h
@@ -20,14 +20,8 @@
 
 #ifndef _FASTLED_NEOMATRIX_H_
 #define _FASTLED_NEOMATRIX_H_
+#include "Framebuffer_GFX.h"
 
-#if ARDUINO >= 100
- #include <Arduino.h>
-#else
- #include <WProgram.h>
- #include <pins_arduino.h>
-#endif
-#include <Adafruit_GFX.h>
 #if defined(ESP8266)
 // If you get matrix flickering, modify platforms/esp/8266/clockless_esp8266.h 
 // and platforms/esp/8266/clockless_block_esp8266.h to change WAIT_TIME to 20
@@ -39,40 +33,6 @@
 #endif
 #include <FastLED.h>
 
-// Matrix layout information is passed in the 'matrixType' parameter for
-// each constructor (the parameter immediately following is the LED type
-// from NeoPixel.h).
-
-// These define the layout for a single 'unified' matrix (e.g. one made
-// from NeoPixel strips, or a single NeoPixel shield), or for the pixels
-// within each matrix of a tiled display (e.g. multiple NeoPixel shields).
-
-#define NEO_MATRIX_TOP         0x00 // Pixel 0 is at top of matrix
-#define NEO_MATRIX_BOTTOM      0x01 // Pixel 0 is at bottom of matrix
-#define NEO_MATRIX_LEFT        0x00 // Pixel 0 is at left of matrix
-#define NEO_MATRIX_RIGHT       0x02 // Pixel 0 is at right of matrix
-#define NEO_MATRIX_CORNER      0x03 // Bitmask for pixel 0 matrix corner
-#define NEO_MATRIX_ROWS        0x00 // Matrix is row major (horizontal)
-#define NEO_MATRIX_COLUMNS     0x04 // Matrix is column major (vertical)
-#define NEO_MATRIX_AXIS        0x04 // Bitmask for row/column layout
-#define NEO_MATRIX_PROGRESSIVE 0x00 // Same pixel order across each line
-#define NEO_MATRIX_ZIGZAG      0x08 // Pixel order reverses between lines
-#define NEO_MATRIX_SEQUENCE    0x08 // Bitmask for pixel line order
-
-// These apply only to tiled displays (multiple matrices):
-
-#define NEO_TILE_TOP           0x00 // First tile is at top of matrix
-#define NEO_TILE_BOTTOM        0x10 // First tile is at bottom of matrix
-#define NEO_TILE_LEFT          0x00 // First tile is at left of matrix
-#define NEO_TILE_RIGHT         0x20 // First tile is at right of matrix
-#define NEO_TILE_CORNER        0x30 // Bitmask for first tile corner
-#define NEO_TILE_ROWS          0x00 // Tiles ordered in rows
-#define NEO_TILE_COLUMNS       0x40 // Tiles ordered in columns
-#define NEO_TILE_AXIS          0x40 // Bitmask for tile H/V orientation
-#define NEO_TILE_PROGRESSIVE   0x00 // Same tile order across each line
-#define NEO_TILE_ZIGZAG        0x80 // Tile order reverses between lines
-#define NEO_TILE_SEQUENCE      0x80 // Bitmask for tile line order
-
 /* 
  * Ideally FastLED_NeoMatrix would multiple inherit from CFastLED too
  * I tried this, but on that path laid madness, apparent compiler bugs
@@ -81,12 +41,9 @@
  * hardcoded at compile time as template values :( -- merlin
  * See https://github.com/marcmerlin/FastLED_NeoMatrix/blob/cd739d471bbbe22336f281f1d1988aa7e7572340/FastLED_NeoMatrix.cpp#L92
  */
-class FastLED_NeoMatrix : public Adafruit_GFX {
+class FastLED_NeoMatrix : public Framebuffer_GFX {
 
- public:
-  // pre-computed gamma table
-  uint8_t gamma[256];
-
+public:
   // Constructor for single matrix:
   FastLED_NeoMatrix(CRGB *, uint8_t w, uint8_t h, 
     uint8_t matrixType = NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS);
@@ -97,53 +54,8 @@ class FastLED_NeoMatrix : public Adafruit_GFX {
     uint8_t matrixType = NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS +
                          NEO_TILE_TOP + NEO_TILE_LEFT + NEO_TILE_ROWS);
 
-
-  int XY(int16_t x, int16_t y); // compat with FastLED code, returns 1D offset
-  void
-    drawPixel(int16_t x, int16_t y, uint16_t color),
-    drawPixel(int16_t x, int16_t y, uint32_t color),
-    drawPixel(int16_t x, int16_t y, CRGB color),
-    fillScreen(uint16_t color),
-    setPassThruColor(CRGB c),
-    setPassThruColor(uint32_t c),
-    setPassThruColor(void),
-    setRemapFunction(uint16_t (*fn)(uint16_t, uint16_t)),
-    precal_gamma(float);
-
-  static uint16_t
-    Color(uint8_t r, uint8_t g, uint8_t b);
-
-  void clear() { fillScreen(0); };
+  void show();
   void setBrightness(int b) { FastLED.setBrightness(b); };
-
-  void show() {
-#ifdef ESP8266
-// Disable watchdog interrupt so that it does not trigger in the middle of
-// updates. and break timing of pixels, causing random corruption on interval
-// https://github.com/esp8266/Arduino/issues/34
-    ESP.wdtDisable();
-#endif
-    FastLED.show();
-#ifdef ESP8266
-    ESP.wdtEnable(1000);
-#endif
-  };
-
-  void begin();
-
- private:
-
-  CRGB *_leds;
-  const uint8_t
-    type;
-  const uint8_t
-    matrixWidth, matrixHeight, tilesX, tilesY;
-  uint16_t
-    numpix,
-    (*remapFn)(uint16_t x, uint16_t y);
-
-  uint32_t passThruColor;
-  boolean  passThruFlag = false;
 };
 
 #endif // _FASTLED_NEOMATRIX_H_
